840ce1558b4cf7cef9f003b942c8c1c721c567e4
anasttas <3333333333333333222n@gmail.com>
2026-05-26 00:58:35 +0300

    Add entities and liquibase changelogs

---
+++ src/main/java/entity/Credential.java
@@ -0,0 +1,30 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "credentials")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Credential {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", unique=true, nullable=false)
+    private Long userId;
+
+    @Column(unique=true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
